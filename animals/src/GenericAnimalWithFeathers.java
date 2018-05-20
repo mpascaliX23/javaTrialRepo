8e70101feb62824cb1090771e7011f588d5496e2
mattia <mpascaliX23@gmail.com>
2018-05-20 18:28:24 +0200

    second commit with push to github repo

---
+++ animals/src/GenericAnimalWithFeathers.java
@@ -0,0 +1,14 @@
+
+public abstract class GenericAnimalWithFeathers extends GenericAnimal {
+	
+	private String featherSize;
+
+	public String getFeatherSizeColor() {
+		return featherSize;
+	}
+
+	public void setFeatherSizeColor(String furColor) {
+		this.featherSize = furColor;
+	}
+
+}
---
+++ animals/src/GenericAnimalWithFur.java
@@ -0,0 +1,14 @@
+
+public abstract class GenericAnimalWithFur extends GenericAnimal {
+	
+	private String furColor;
+
+	public String getFurColor() {
+		return furColor;
+	}
+
+	public void setFurColor(String furColor) {
+		this.furColor = furColor;
+	}
+
+}
